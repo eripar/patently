d1891fead0fab122c570dfc7fbd477b0a3a998df
eripar <52082968+eripar@users.noreply.github.com>
2019-07-14 19:59:19 -0400

    v0.3
    Cleaned app layout, added storing of the users queries, removed stemming, split index/analyze templates, added feedback survey and Google Analytics tag.

--- flask-app/app.py
+++ flask-app/app.py
@@ -2,6 +2,7 @@
 # Import Flask and pandas
 from flask import Flask, render_template, request
 import pandas as pd
+import datetime
 
 # Import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -11,6 +12,8 @@ from sklearn.metrics.pairwise import linear_kernel
 import spacy
 import string
 from spacy.lang.en import English
+from nltk.stem.porter import *
+stemmer = PorterStemmer()
 
 # Load the spaCy english library, punctuations, and stopwords
 nlp = spacy.load('en_core_web_sm')
@@ -24,7 +27,8 @@ nlp.Defaults.stop_words |= {"electrochemical","electrochemistry","use","comprise
                             "produce","contain"}
 
 # Read the patent dataset
-df = pd.read_csv("data/export_patents_view_main_tokenized_v2.csv",)
+df_user_search= pd.read_csv("data/user_search.csv",)
+df = pd.read_csv("data/export_patent_view_main_tokenized_v2.csv",)
 df_descriptions= pd.read_csv("data/class_descriptions.csv",)
 copy_df = df
 
@@ -38,36 +42,38 @@ length = len(docs)
 app = Flask(__name__)
 
 # Function to process the user's input into tokens that can be compared against the tokenized patent abstract data
-def spacy_tokenizer(docs): 
-    # initialize empty lists/series
-    tk_nostop = []
-    tk_nostop_pos = []
-    df_tokens = pd.Series()
+def spacy_tokenizer(docs):  
+    mytokens = []
+    mytokens_out = []
+    print_tokens = []
     mytokens = parser( docs )
-    # Process text NO1: lowercase, remove spaces, and pronouns
+    # Process text NO1: lowercase, remove spaces, and lemmatize
     mytokens = [ word.lemma_.lower().strip() if word.lemma_ !='-PRON-' else word.lower_ for word in mytokens ]
     # Process text NO2: remove stop words and punctuations
     mytokens = [ word for word in mytokens if word not in stop_words and word not in punctuations ] 
-    token_nlp = nlp( str( mytokens )) 
-    #if word  is not a stopword, then allow thru the system
-    for word in token_nlp:
-        if word.is_stop==False:  
-            tk_nostop.append(word)
-    for word in tk_nostop:
-        tk_word_thru = (word.pos_!='VERB') & (word.pos_!='PUNCT') & (word.pos_!='CCONJ') & (word.pos_!='ADV') & (word.pos_!='NUM') & (word.pos_!='X')
-        if tk_word_thru:
-            tk_nostop_pos.append(word)
-    dum_series = pd.Series(str(tk_nostop_pos))
-    df_tokens = df_tokens.append(dum_series, ignore_index=True)
-    return df_tokens
+    
+    mytokens = nlp( str( mytokens )) 
+    for word in mytokens: 
+        #remove verbs, punctuation, conjunctions, adverbs, numbers, and undefined parts-of-speech
+        mytoken = (word.pos_!='VERB') & (word.pos_!='PUNCT') & (word.pos_!='CCONJ') & (word.pos_!='ADV') & (word.pos_!='NUM') & (word.pos_!='X')
+        if mytoken:
+            print_tokens.append(word)
+            #stem
+            #word=stemmer.stem(word.text)
+            mytokens_out.append(word)
+            
+    mytokens_out = pd.Series(str(mytokens_out))
+    return mytokens_out, print_tokens
+
 
 # initial page front
 @app.route("/")
 def home():
     return render_template("index.html.j2")
 
+
 # page that returns after a user "submits"
-@app.route("/", methods=['POST'] )
+@app.route("/analyze", methods=['POST'] )
 def analyze():
     copy_df = df
     if request.method == "POST":
@@ -76,8 +82,16 @@ def analyze():
         n = request.form["result_count"]
         n = int(n)
         received_text = rawtext
+        
+        #store user search for analysis
+        df_user_search= pd.read_csv("data/user_search.csv",)
+        df_store = pd.DataFrame( {'timestamp':[datetime.datetime.now()], 'user_input':[rawtext] } )
+        df_user_search = pd.concat([df_user_search, df_store], ignore_index=True, sort=True)
+        df_user_search = df_user_search[['timestamp', 'user_input']]
+        df_user_search.to_csv("data/user_search.csv",)
+        
         #convert input into tokens for the user to see
-        text_tokens = spacy_tokenizer(received_text)
+        text_tokens, print_tokens = spacy_tokenizer(received_text)
         #vectorizer user input
         user_input = pd.Series(text_tokens)
         #transform tokenized query into TFIDF
@@ -96,7 +110,7 @@ def analyze():
         #convert dataframe into dictionary
         out_df = out_df.to_dict('records')
         
-    return render_template('index.html.j2', received_text=received_text, text_tokens=text_tokens[0], table=out_df )
+    return render_template('analyze.html.j2', received_text=rawtext, text_tokens=print_tokens[:], table=out_df )
 
 #https://www.websiteout.net/counter.php
 
